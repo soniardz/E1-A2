47c1987819db1e8f7e2685d04dc2f0f2e75ca087
Sonia Rodríguez <65564642+soniardz@users.noreply.github.com>
2020-06-02 19:50:32 -0500

    E2 A1
    Creación de un nuevo componente y uso de ngFor.

---
+++ src/app/list/list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Automovil } from '../models';
+import { AUTOMOVILES } from '../data';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit {
+
+  autos: Automovil[];
+  constructor() { }
+
+  ngOnInit(): void {
+    this.autos=AUTOMOVILES;
+  }
+
+}
