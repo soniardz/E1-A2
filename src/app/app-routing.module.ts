93075f89b066965aee64a83a556711290d9ec291
Sonia Rodríguez <65564642+soniardz@users.noreply.github.com>
2020-06-16 00:58:47 -0500

    E3 A6
    Page not Found y rutas por default

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListComponent } from './list/list.component';
 import { TableComponent } from './table/table.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
-
+  {
+    path: '', 
+    redirectTo: '/table',
+    pathMatch: 'full'
+  },
   {
     path: 'list', 
     component: ListComponent
@@ -13,6 +18,10 @@ const routes: Routes = [
   {
     path: 'table',
     component: TableComponent
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
   }
 
 ];
